b7b084cfa8e0435159208c2030326b733075c059
Florian <f.klein@protonmail.com>
2023-07-13 15:21:36 +0200

    added all accessible gps data csv's to data folder, finshed csv reader and qml coordinate replacer

---
+++ poc/qmladjuster.py
@@ -0,0 +1,19 @@
+import re
+
+def replace_qml_coordinates(file_path, lat, lon):
+    # pattern = r'QtPositioning\.coordinate\(\d+\.\d+, \d+\.\d+\)'
+    pattern = r'QtPositioning\.coordinate\(([-+]?\d*\.\d+|\d+), ([-+]?\d*\.\d+|\d+)\)'
+    # ([-+]?\d*\.\d+|\d+) matches positive and negative floats or decimials
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    replaced_content, number_of_replacements = re.subn(pattern, f'QtPositioning.coordinate({lat:.6f}, {lon:.6f})', content)
+
+    print(number_of_replacements)
+
+    with open(file_path, 'w') as file:
+        file.write(replaced_content)
+
+if __name__ == "__main__":
+    replace_qml_coordinates("map_dummy_for_qmladjuster.qml", 5.0, 5.0)
